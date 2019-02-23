f2e090b32efd7d6f2bd1dbc36645d5baf439a372
TheNormalnij <zarevi4vlad@gmail.com>
2019-02-23 19:54:35 +0300

    RenderTarget: dxDrawText handler

--- dxGUI/RenderTarget.lua
+++ dxGUI/RenderTarget.lua
@@ -6,15 +6,20 @@ local drawingOffsetsEnabled = false
 local dxSetRenderTarget = dxSetRenderTarget
 
 local _dxDrawImage = dxDrawImage
+local _dxDrawText = dxDrawText
 
 local function setDrawingOffset( offX, offY )
 	if offX and offY then
 		dxDrawImage = function( x, y, ... )
 			_dxDrawImage( x - offX, y - offY, ... )
 		end
+		dxDrawText = function( text, x1, y1, x2, y2, ... )
+			_dxDrawText( text, x1 - offX, y1 - offY, x2 - offX, y2 - offY, ... )
+		end
 		drawingOffsetsEnabled = true
 	else
 		dxDrawImage = _dxDrawImage
+		dxDrawText = _dxDrawText
 		drawingOffsetsEnabled = false
 	end
 end
