635c4642f003d49edea938a3eb2a53239de94f0d
Enes Bayraktar <steam.enesbayraktar@gmail.com>
2020-12-23 22:43:49 +0300

    Add textShadow feature, fixes some bugs

--- dxGUI/TextBordered.lua
+++ dxGUI/TextBordered.lua
@@ -1,14 +1,25 @@
--- from https://wiki.multitheftauto.com/wiki/DxDrawBorderedText
 dxGUI.text:subclass{
-	type       = 'textBordered';
-	outline    = 2;
+  type       = 'textBordered';
+  outline    = 2;
+  color      = 0xFFFFFFFF;
+  alignX     = 'center';
+  alignY     = 'center';
+  clip       = false;
+  wordBreak  = false;
+  postGUI    = false;
+  colorCoded = false;
+  unicodeText = false;
+  shadowColor = tocolor(0, 0, 0, 225);
 
-	draw = function( self )
-		for oX = (self.outline * -1), self.outline do
+  draw = function( self )
+    local _, _, _, alpha = color.HEXtoRGB( self.color )
+    local r, g, b, shadowAlpha = color.HEXtoRGB( self.shadowColor )
+    shadowColor = tocolor(r, g, b, shadowAlpha * alpha / 255)
+    for oX = (self.outline * -1), self.outline do
 			for oY = (self.outline * -1), self.outline do
-				dxDrawText(self.text, self.x + oX, self.y + oY, self.x + self.w + oX, self.y + self.h + oY, 0xFF000000, self.scale, self.font, self.alignX, self.alignY, self.clip, self.wordBreak, self.postGUI, self.colorCoded)
-			end
+				dxDrawText(string.gsub(self.text, '#%x%x%x%x%x%x', ''), self.x + oX, self.y + oY, self.x + self.w + oX, self.y + self.h + oY, shadowColor, self.scale, self.font, self.alignX, self.alignY, self.clip, self.wordBreak, self.postGUI, false)
+	 		end
 		end
-		dxDrawText (self.text, self.x, self.y, self.x + self.w, self.y + self.h, self.color, self.scale, self.font, self.alignX, self.alignY, self.clip, self.wordBreak, self.postGUI, self.colorCoded)
-	end;
+		dxDrawText(self.text, self.x, self.y, self.x + self.w, self.y + self.h, self.color, self.scale, self.font, self.alignX, self.alignY, self.clip, self.wordBreak, self.postGUI, self.colorCoded)
+  end;
 }
